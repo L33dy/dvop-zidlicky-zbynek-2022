310d72002b9e73d0d5c490dffaaa2509f93baf9a
L33dy <zbynekzidlicky@gmail.com>
2023-02-24 17:52:38 +0100

    svelte-hw updatos

--- individual_work/svelte-hw/myapp/src/routes/+page.svelte
+++ individual_work/svelte-hw/myapp/src/routes/+page.svelte
@@ -1,16 +1,61 @@
 <script>
-    import Component from "./Component.svelte";
+    import Counter from "./Counter.svelte";
 
-    let counter = 0;
+    let user = { loggedIn: false }
 
-    function add() {
-        counter++
+    function toggle() {
+        user.loggedIn = !user.loggedIn
     }
 </script>
 
-<Component>
-</Component>
+<main>
+    {#if user.loggedIn}
+        <h1>User Environment</h1>
 
-<h1>{counter}</h1>
-<button on:click={add}>Click</button>
+        <Counter></Counter>
 
+        <button on:click={toggle}>
+            Log Out
+        </button>
+
+    {:else}
+
+        <button on:click={toggle}>
+            Log In
+        </button>
+    {/if}
+</main>
+
+<style>
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+
+        width: 100%;
+        height: 100vh;
+    }
+
+    button {
+        cursor: pointer;
+
+        background-color: #637aff;
+
+        width: 135px;
+        height: 52px;
+
+        border-radius: 8px;
+        border: none;
+
+        transition: 200ms;
+
+        color: white;
+
+        font-size: 1.2rem;
+    }
+
+    button:hover {
+        background-color: #4756bb;
+    }
+</style>
